ca8f7c57c6c3811432b6f414c3848ecc8e1d0cae
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 14:43:57 -0800

    Update SampleClass.groovy

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -6,7 +6,15 @@ class SampleClass {
    Integer age
 
    def increaseAge(Map mymap) {
-      System.out.println "Invoke method $mymap.name"
+      def map = [
+    'FF0000' : 'Red',
+    '00FF00' : 'Lime',
+    '0000FF' : 'Blue',
+    'FFFF00' : 'Yellow'
+]
+      map.each { println "Hex Code: $it.key = Color Name: $it.value" }
+      
+    //  System.out.println "Invoke method $mymap.name"
       //this.age += years
    }
 }
