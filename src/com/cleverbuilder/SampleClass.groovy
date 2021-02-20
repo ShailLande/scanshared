93f2876eab9c8b58d14b88c8998415cf8ff5543c
Shail <shail.lande@venafi.com>
2021-02-20 12:07:58 -0800

    test

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -6,7 +6,7 @@ class SampleClass {
    Integer age
 
    def increaseAge(Map mymap) {
-      System.out.println "Invoke method $mymap.name
+      System.out.println "Invoke method $mymap.name"
       //this.age += years
    }
 }
