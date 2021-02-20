a630e8345d086cdf5899cd08b5ceec0546851ec9
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 15:39:16 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(String name,int age,Map m) {
-  echo "Hello, ${name},${age}, ${m}"
+  echo "Hello, ${name},${age}"
+  m.each{entry -> println "$entry.key: $entry.value"}
+ // docker pull aquasec/trivy
 }
 
