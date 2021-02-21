d9022b8bd0537db536e710bff50ca484800dfc52
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 23:31:13 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -6,10 +6,16 @@ def call(String name,int age,Map m) {
  // helloWorldExternal(name: "shail", dayOfWeek: "Sunday")
   sh(returnStdout: true, script: 'docker pull aquasec/trivy')
   sh(returnStdout: true, script: 'docker pull venafidevops/venafi-java-base')
-  def rc = sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image  venafidevops/venafi-java-base')
-  echo "RCCCC - ${rc}"
+  return sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image  venafidevops/venafi-java-base')
+//  echo "RCCCC - ${rc}"
+  
  //docker pull venafidevops/venafi-java-base
 // docker run --rm aquasec/trivy image  venafidevops/venafi-java-base 
+  
+  //package com.cleverbuilder
+
+//class GlobalVars {
+//   static String foo = "bar"
  
  
 } 
