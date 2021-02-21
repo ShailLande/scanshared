a608a75443f3fe98efa808d1788a4f74bdcaa4de
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 00:18:48 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String name,int age,Map m,SampleClass sc) {
+def call(String name,int age,Map m,com.cleverbuilder.SampleClass sc) {
   echo "Hello, ${name},${age}"
   m.each{entry -> println "$entry.key: $entry.value"}
  // helloWorldExternal(name: "shail", dayOfWeek: "Sunday")
