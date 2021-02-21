96eca2c750810a4abafa5c0ae493234437e5a200
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 23:19:07 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,8 +3,11 @@
 def call(String name,int age,Map m) {
   echo "Hello, ${name},${age}"
   m.each{entry -> println "$entry.key: $entry.value"}
-  helloWorldExternal(name: "shail", dayOfWeek: "Sunday")
- 
+ // helloWorldExternal(name: "shail", dayOfWeek: "Sunday")
+  def rc = sh(returnStdout: true, script: 'docker pull aquasec/trivy')
+  echo "RCCCC - ${rc}"
+ //docker pull venafidevops/venafi-java-base
+// docker run --rm aquasec/trivy image  venafidevops/venafi-java-base 
  
  
 } 
