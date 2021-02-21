d7679bcf12cb8f1327a59506313337077c1b757d
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 16:33:11 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String name,int age,Map m) {
+def int call(String name,int age,Map m) {
   echo "Hello, ${name},${age}"
   m.each{entry -> println "$entry.key: $entry.value"}
   sh '''
@@ -8,9 +8,9 @@ def call(String name,int age,Map m) {
  docker pull venafidevops/venafi-java-base
  docker run --rm aquasec/trivy image  venafidevops/venafi-java-base 
  exitcode=$?
-
+'''
  
-  '''
+ 
 } 
  return exitcode
 
