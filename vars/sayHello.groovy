fa8d1ece9d5b3327771eda50ef467c089254cf5f
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 12:15:06 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,15 +1,14 @@
 #!/usr/bin/env groovy
 
-def call(String name,int age,Map m,com.cleverbuilder.SampleClass sc) {
-  echo "Hello, ${name},${age}"
-  m.each{entry -> println "$entry.key: $entry.value"}
+def call(String imgname,Map m) {
+//  echo "Hello, ${name},${age}"
+//  m.each{entry -> println "$entry.key: $entry.value"}
  // helloWorldExternal(name: "shail", dayOfWeek: "Sunday")
   sh(returnStdout: true, script: 'docker pull aquasec/trivy')
   sh(returnStdout: true, script: 'docker pull venafidevops/venafi-java-base')
-  def rc= sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image  venafidevops/venafi-java-base')
-  echo "RCCCC - ${rc}"
- // def sc= new com.cleverbuilder.SampleClass()
-  sc.code = "${rc}"
+  def rc= sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image' + ${imgname} + ')'  //venafidevops/venafi-java-base')
+ // echo "RCCCC - ${rc}"
+
   return "${rc}"
    // def consul =  new org.foo.Consul()
   //  consul.construct(httpObj)
