88347c48aed370920d69c9513857fd98e9a195a4
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 23:11:49 -0800

    Update script.sh

--- resources/com/planetpope/scripts/linux/script.sh
+++ resources/com/planetpope/scripts/linux/script.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Test redirect"
+echo "Test start **************"
+ docker pull aquasec/trivy
+ docker pull venafidevops/venafi-java-base
+ docker run --rm aquasec/trivy image  venafidevops/venafi-java-base 
+ echo "Test done *******************"
