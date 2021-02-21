ff3c61120751474efd287746cde0f9c515447f95
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 23:03:54 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 
-def int call(String name,int age,Map m) {
+def call(String name,int age,Map m) {
   echo "Hello, ${name},${age}"
   m.each{entry -> println "$entry.key: $entry.value"}
-  helloWorldExternal
+  helloWorldExternal(name: "shail", dayOfWeek: "Sunday")
  
  
  
 } 
- return exitcode
+
 
