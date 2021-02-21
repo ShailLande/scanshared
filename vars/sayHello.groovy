928839d8e774de6f2b380dd65efc41856e1720ef
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 12:17:06 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -7,9 +7,10 @@ def call(String imgname,Map m) {
   sh(returnStdout: true, script: 'docker pull aquasec/trivy')
   sh(returnStdout: true, script: 'docker pull venafidevops/venafi-java-base')
   def rc= sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image' + "${imgname}" + ')'  //venafidevops/venafi-java-base')
- // echo "RCCCC - ${rc}"
+  echo "RCCCC - ${rc}"
 
-  return "${rc}"
+//  return "${rc}"
+             return 0
    // def consul =  new org.foo.Consul()
   //  consul.construct(httpObj)
  //   return consul
