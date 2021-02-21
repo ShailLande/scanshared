e47e96d769dafb734d7f010158b9dde1afc67f76
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 22:56:04 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,12 +3,8 @@
 def int call(String name,int age,Map m) {
   echo "Hello, ${name},${age}"
   m.each{entry -> println "$entry.key: $entry.value"}
-  sh '''
- docker pull aquasec/trivy
- docker pull venafidevops/venafi-java-base
- docker run --rm aquasec/trivy image  venafidevops/venafi-java-base 
- exitcode=$?
-'''
+  helloWorldExternal
+ 
  
  
 } 
