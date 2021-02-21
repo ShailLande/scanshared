78592a694452a77087cfb1f398f4959a1f4b6c46
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-20 23:09:29 -0800

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,4 +1,5 @@
 def call(Map config = [:]) { 
+  echo "tttttt ${config.name}"
   def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
   sh "chmod a+x ./${config.name}"
