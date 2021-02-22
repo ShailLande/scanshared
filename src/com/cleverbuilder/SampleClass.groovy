5132a09a6910224c773d0d995d396960ed947756
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 22:48:16 -0800

    Update SampleClass.groovy

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -5,13 +5,13 @@ class SampleClass {
    String name
    Integer code
 
-   def Integer increaseAge(String val) {
+   def increaseAge(String val) {
      
-       sh(returnStdout: true, script: 'docker pull aquasec/trivy')
+      return sh(returnStdout: true, script: 'docker pull aquasec/trivy')
  // sh(returnStdout: true, script: 'docker pull venafidevops/venafi-java-base')
 //  def rc= sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image  venafidevops/venafi-java-base')
  //     this.code= ${rc}
-     return 1
+    // return 1
      // this.name = env['VARIABLE']
    }
 }
