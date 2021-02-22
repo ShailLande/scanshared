1d10a37ae768a14c7877d489b6ec4e25fc265686
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 16:52:55 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,8 +2,7 @@
 
 def call(String imgname,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD) {
   echo "Hello, ${imgname}"
-  String[] str = imgname.split(':');
-  sh 'docker login https://${SRC_DOCKER_REGISTRY} -u ${ARTIFACTORY_USERNAME} -p ${ARTIFACTORY_PASSWD}'
+  sh 'docker login https://${SRC_DOCKER_REGISTRY} -u shail.lande -p Ganeshya@112129'
   sh(returnStdout: true, script: 'docker pull aquasec/trivy')
   sh(returnStdout: true, script: "docker pull ${imgname}")
   def rc = sh(script: "docker run --rm aquasec/trivy image ${imgname}")//\\\"${since}\\\""
