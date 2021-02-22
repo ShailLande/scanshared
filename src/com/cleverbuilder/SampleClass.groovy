22639ac0363eb2af6422098d01677070c88fad2c
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 17:12:34 -0800

    Update SampleClass.groovy

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -5,21 +5,14 @@ class SampleClass {
    String name
    Integer code
 
-   def increaseAge(String val) {
-     /* def map = [
-    'FF0000' : 'Red',
-    '00FF00' : 'Lime',
-    '0000FF' : 'Blue',
-    'FFFF00' : 'Yellow'
-]
-      map.each { println "Hex Code: $it.key = Color Name: $it.value" }
-    */ 
-     // def proc ='./script.sh'.execute()
+   def integer increaseAge(String val) {
+    
+    
         sh(returnStdout: true, script: 'docker pull aquasec/trivy')
-  sh(returnStdout: true, script: 'docker pull venafidevops/venafi-java-base')
-  def rc= sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image  venafidevops/venafi-java-base')
-      this.code= ${rc}
-     
+ // sh(returnStdout: true, script: 'docker pull venafidevops/venafi-java-base')
+//  def rc= sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image  venafidevops/venafi-java-base')
+ //     this.code= ${rc}
+     return 1
      // this.name = env['VARIABLE']
    }
 }
