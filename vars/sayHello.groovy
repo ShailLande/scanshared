2a4d42ab828599679839d611a30c5a1e611016eb
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 16:48:37 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,36 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(String imgname,Map m) {
+def call(String imgname,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD) {
   echo "Hello, ${imgname}"
   String[] str = imgname.split(':');
-  //for( String values : str )
-   //   println(values);
-//  m.each{entry -> println "$entry.key: $entry.value"}
- // helloWorldExternal(name: "shail", dayOfWeek: "Sunday")
+  sh 'docker login https://${SRC_DOCKER_REGISTRY} -u ${ARTIFACTORY_USERNAME} -p ${ARTIFACTORY_PASSWD}'
   sh(returnStdout: true, script: 'docker pull aquasec/trivy')
   sh(returnStdout: true, script: "docker pull ${imgname}")
-  //echo "RCCCC - ${rc}"
- 
- // sh("${cmd}")//returnStdout: true,script: 'docker run --rm aquasec/trivy image ${imgname}')//
- // docker run --rm aquasec/trivy image  venafidevops/venafi-java-base 
-//  echo "cmddd-  ${cmd}"
-  /*
-  sh """
-  docker run --rm aquasec/trivy image ${imgname}
-    """
-    */
-
   def rc = sh(script: "docker run --rm aquasec/trivy image ${imgname}")//\\\"${since}\\\""
-//  def rc= sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image' + "${imgname}" + ')'  //venafidevops/venafi-java-base')
-  //echo "RCCCC - ${rc}"
-
-  //return 1
   return "${rc}"
-   // def consul =  new org.foo.Consul()
-  //  consul.construct(httpObj)
- //   return consul
-//  com.cleverbuilder.GlobalVars.foo = "ssss"
-
-} 
+  } 
 
 
