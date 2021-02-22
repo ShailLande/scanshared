c470f56621b233de6b6c773949c8d03346aeaa95
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 22:56:19 -0800

    Update SampleClass.groovy

--- src/com/cleverbuilder/SampleClass.groovy
+++ src/com/cleverbuilder/SampleClass.groovy
@@ -7,7 +7,7 @@ class SampleClass {
 
    def increaseAge(String val) {
      
-      return sh(script: "docker pull aquasec/trivy")
+      return sh("script: docker pull aquasec/trivy")
  // sh(returnStdout: true, script: 'docker pull venafidevops/venafi-java-base')
 //  def rc= sh(returnStatus: true, script: 'docker run --rm aquasec/trivy image  venafidevops/venafi-java-base')
  //     this.code= ${rc}
