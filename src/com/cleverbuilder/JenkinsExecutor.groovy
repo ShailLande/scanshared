6c77dede210d0989bb06e9f0f8547615c68a1dc6
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-21 17:44:19 -0800

    Create JenkinsExecutor.groovy

---
+++ src/com/cleverbuilder/JenkinsExecutor.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+package com.cleverbuilder
+
+interface JenkinsExecutor extends Serializable {
+    void stage(String name, Closure config)
+    String sh(String command)
+    void echo(String message)
+    ...
+}
