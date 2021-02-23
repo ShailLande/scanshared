07984c1e48692f6a2f39ae95cd5c0aa97c977ee9
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-22 18:00:25 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,10 +3,38 @@
 def call(String imgname,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD) {
   echo "Hello, ${imgname}"
   //sh 'docker login https://${SRC_DOCKER_REGISTRY} -u ${ARTIFACTORY_USERNAME} -p ${ARTIFACTORY_PASSWD}'
+  try
+  {
   sh(returnStdout: true, script: 'docker pull aquasec/trivy')
-  sh(returnStdout: true, script: "docker pull ${imgname}")
-  def rc = sh(script: "docker run --rm aquasec/trivy image ${imgname}")//\\\"${since}\\\""
-  return "${rc}"
+  }
+  catch (Exception e)
+  {
+    println("Exception: ${e}")
+    return 1
+  }
+  
+  try
+  {
+   sh(returnStdout: true, script: "docker pull ${imgname}")
+  }
+  catch (Exception e)
+  {
+    println("Exception: ${e}")
+    return 1
+  }
+  
+  try
+  {
+    def rc = sh(script: "docker run --rm aquasec/trivy image ${imgname}")
+  }
+  catch (Exception e)
+  {
+    println("Exception: ${e}")
+    return 1
+  }
+ 
+
+  return 0
   } 
 
 
