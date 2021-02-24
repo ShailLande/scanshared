42d5343d8f3b7b9e8eacaf90b9c13125a99740af
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-24 11:50:05 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,58 +1,8 @@
-#!/usr/bin/env groovy
-
-
 def call(String imgname,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD) {
   echo "Hello, ${imgname}"
   //sh 'docker login https://${SRC_DOCKER_REGISTRY} -u ${ARTIFACTORY_USERNAME} -p ${ARTIFACTORY_PASSWD}'
-  try
-  {
   sh(returnStdout: true, script: 'docker pull aquasec/trivy')
-  }
-  catch (Exception e)
-  {
-    println("Exception in Docker Pull: ${e}")
-  }
-  
-  try
-  {
-   sh(returnStdout: true, script: "docker pull ${imgname}")
-  }
-  catch (Exception e)
-  {
-    println("Exception in Docker pull Image : ${e}")
-  
-  }
-  
-  def rc=-1
-  try
-  {
-    rc = sh(script: "docker run --rm aquasec/trivy --exit-code 3 --severity CRITICAL,HIGH ${imgname}")
-  }
-  catch (Exception e)
-  {
-    println("Exception in Docker Run Image: ${e}")
-    return 1
-  }
- 
+  sh(returnStdout: true, script: "docker pull ${imgname}")
+  def rc = sh(script: "docker run --rm aquasec/trivy image ${imgname}")//\\\"${since}\\\""
   return "${rc}"
   } 
-
-
-/*
- 
-  sh(returnStdout: true, script: 'docker pull aquasec/trivy')
- 
-  
- 
-   sh(returnStdout: true, script: "docker pull ${imgname}")
-
-  
-
-    def rc = sh(script: "docker run --rm aquasec/trivy image ${imgname}")
-   
-
-  return "${rc}"
-  } 
-  */
-
-
