e16a4546b958f67917bdaed0b4e2d871a9fa7638
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-25 23:00:35 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -13,7 +13,7 @@ def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_
   else
   rc = sh(returnStatus:true,label:"Testing Img" ,script: "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --severity CRITICAL,HIGH ${imgname}")
  // rcmap["${imgname}"]="${rc}"
-    rcmap["venafidevops/venafi-java-base"]="${rc}"
+    rcmap["img1"]="${rc}"
   if("${rc}" == "1") 
     echo "Build failed due to HIGH or CRITICAL Vulnerabilties for Image- ${imgname}"
   else
