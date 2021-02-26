a983def1c7452d4e5a76b19977fc87cd63a740d8
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-25 22:46:14 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -12,7 +12,7 @@ def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_
   rc = sh(returnStatus:true,label:"Testing Img" ,script: "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --exit-code 1 --severity CRITICAL,HIGH ${imgname}")
   else
   rc = sh(returnStatus:true,label:"Testing Img" ,script: "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --severity CRITICAL,HIGH ${imgname}")
-    rcmap["${imgname}"]="${rc}"
+  rcmap["${imgname}"]="${rc}"
   if("${rc}" == "1") 
     echo "Build failed due to HIGH or CRITICAL Vulnerabilties for Image- ${imgname}"
   else
@@ -20,6 +20,7 @@ def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_
     
     
   }
+  echo "RCMAPPP in sayhello - ${rcmap}"
   return "${rcmap}"
   
   } 
