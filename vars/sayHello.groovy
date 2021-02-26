7cadaca723bbc461053c73cbb0116b35d8b291aa
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-25 23:52:26 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -26,6 +26,6 @@ def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_
   }
   echo "RCMAPPP in sayhello - ${rcmap}"
   rcmap.each{entry -> println "$entry.key: $entry.value"}
-  return(${rcmap})
+  return(rcmap)
   
   } 
