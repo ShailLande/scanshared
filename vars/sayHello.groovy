cf001125cf92613a8020159dd261fbf5c0ecbb84
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-25 23:30:00 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,8 @@
 import groovy.json.JsonSlurper 
+import java.util.HashMap;
 def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD,String Branch) {
-  def rcmap = [:]
+ //def rcmap = [:]
+  def HashMap<String, String> rcmap = new HashMap<String, String>();
   for(imgname in myimglist)
   {
    echo "Running scan for - ${imgname}"
@@ -13,7 +15,8 @@ def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_
   else
   rc = sh(returnStatus:true,label:"Testing Img" ,script: "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --severity CRITICAL,HIGH ${imgname}")
  // rcmap["${imgname}"]="${rc}"
-    rcmap["img1"]="${rc}"
+ //   rcmap["img1"]="${rc}"
+    rcmap.put("England", "London");
   if("${rc}" == "1") 
     echo "Build failed due to HIGH or CRITICAL Vulnerabilties for Image- ${imgname}"
   else
