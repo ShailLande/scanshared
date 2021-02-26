cceb4c86b7c05388e7e6bf3613df368c3248dd6b
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-02-25 21:40:15 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,16 +1,19 @@
 import groovy.json.JsonSlurper 
-def call(String imgname,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD) {
+def call(String imgname,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD,String Branch) {
   echo "Hello, ${imgname}"
   //sh 'docker login https://${SRC_DOCKER_REGISTRY} -u ${ARTIFACTORY_USERNAME} -p ${ARTIFACTORY_PASSWD}'
   sh(returnStdout: true, script: 'docker pull aquasec/trivy')
   sh(returnStdout: true, script: "docker pull ${imgname}")
-  def rc = sh(returnStatus:true,label:"Testing Img" ,script: "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --exit-code 1 --severity CRITICAL,HIGH ${imgname}")
+  def rc="-1"
+  if (Branch == "master")
+  rc = sh(returnStatus:true,label:"Testing Img" ,script: "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --exit-code 1 --severity CRITICAL,HIGH ${imgname}")
+  else
+  rc = sh(returnStatus:true,label:"Testing Img" ,script: "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --severity CRITICAL,HIGH ${imgname}")
+  
   if("${rc}" == "1") 
    echo "Build failed due to HIGH or CRITICAL Vulnerabilties for Image- ${imgname}"
   else
-   echo "Build Passed for Image - ${imgname}" 
+   echo "Build Scanned for Image - ${imgname}" 
   
-  def task = "python main.py".execute()
-  task.waitFor()
-  println task.text
+  
   } 
