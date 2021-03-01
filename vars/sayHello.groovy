32d2fcf0f88bd1aae1106132896d84abaa089253
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-03-01 09:39:08 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -11,7 +11,7 @@ def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_
   sh(returnStdout: true, script: "docker pull ${imgname}")
   def rc="-1"
   if (Branch == "master")//--exit-code 1
-  rc = sh(returnStatus:true,label:"Scanning Image" ,script: "docker run  --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy  --severity CRITICAL,HIGH ${imgname}")
+  rc = sh(returnStatus:true,label:"Scanning Image" ,script: "docker run  --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy  exit-code 1 --severity CRITICAL,HIGH ${imgname}")
   else
   rc = sh(returnStatus:true,label:"Scanning Image" ,script: "docker run  --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --severity CRITICAL,HIGH ${imgname}")
    echo "for ${imgname} -----${rc}"
