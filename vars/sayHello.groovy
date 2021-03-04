c930c62f01d76c56b8a628b59e98bc9bbe91e635
ShailLande <79352816+ShailLande@users.noreply.github.com>
2021-03-04 09:10:22 -0800

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,9 +1,6 @@
-import groovy.json.JsonSlurper 
-import java.util.HashMap;
-//def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD,String Branch) {
- def call(java.util.List myimglist,String Branch) {
- echo "in sayhello"
- def rcmap = []
+
+def call(java.util.List myimglist,String SRC_DOCKER_REGISTRY,String ARTIFACTORY_USERNAME,String ARTIFACTORY_PASSWD,String Branch) {
+ def rcmap = [:]
  // def HashMap<String, String> rcmap = new HashMap<String, String>();
   for(imgname in myimglist)
   {
@@ -17,18 +14,18 @@ import java.util.HashMap;
   else
   rc = sh(returnStatus:true,label:"Scanning Image" ,script: "docker run  --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy --severity CRITICAL,HIGH ${imgname}")
    echo "for ${imgname} -----${rc}"
-   def imrc="${imgname}" +"--" +  "${rc}"
-   rcmap.add(imrc)
-  //rcmap["${imgname}"]=rc
+  rcmap["${imgname}"]=rc
  //   rcmap["img1"]="${rc}"
  //   rcmap.put("England", "London");
   if("${rc}" == "1") 
     echo "Build failed due to HIGH or CRITICAL Vulnerabilties for Image- ${imgname}"
   else
    echo "Build Scanned for Image - ${imgname}" 
+    
+    
   }
- // echo "RCMAPPP in sayhello - ${rcmap}"
- // rcmap.each{entry -> println "KEYVALLLL -$entry.key: $entry.value"}
+  echo "RCMAPPP in sayhello - ${rcmap}"
+  rcmap.each{entry -> println "KEYVALLLL -$entry.key: $entry.value"}
   return(rcmap)
   
   } 
